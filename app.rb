e514dedbdf86f378a7940dcee9f866f092acd47c
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-09 16:36:36 -0400

    Move the codes hash to a separate YAML file so it can be parsed in the tests

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require 'yaml'
+
 def any(url, verbs = %w(get post put delete options patch), &block)
   verbs.each do |verb|
     send(verb, url, &block)
@@ -8,55 +10,7 @@ def any(url, verbs = %w(get post put delete options patch), &block)
 end
 
 class MockIssues < Sinatra::Base
-  codes = {
-    '200' => 'Successful',
-    '300' => 'Multiple Choices',
-    '301' => 'Moved Permanently',
-    '302' => 'Found',
-    '303' => 'See Other',
-    '304' => 'Not Modified',
-    '305' => 'Use Proxy',
-    '306' => 'Switch Proxy',
-    '307' => 'Temporary Redirect',
-    '308' => 'Permanent Redirect',
-    '400' => 'Bad Request',
-    '401' => 'Unauthorized',
-    '402' => 'Payment Required',
-    '403' => 'Forbidden',
-    '404' => 'Not Found',
-    '405' => 'Method Not Allowed',
-    '406' => 'Not Acceptable',
-    '407' => 'Proxy Authentication Required',
-    '408' => 'Request Timeout',
-    '409' => 'Conflict',
-    '410' => 'Gone',
-    '411' => 'Length Required',
-    '412' => 'Precondition Failed',
-    '413' => 'Request Entity Too Large',
-    '414' => 'Request-URI Too Long',
-    '415' => 'Unsupported Media Type',
-    '416' => 'Requested Range Not Satisfiable',
-    '417' => 'Expectation Failed',
-    '418' => 'I\'m a teapot',
-    '422' => 'Unprocessable Entity',
-    '423' => 'Locked',
-    '424' => 'Failed Dependency',
-    '425' => 'Unordered Collection',
-    '426' => 'Upgrade Required',
-    '449' => 'Retry With',
-    '450' => 'Blocked by Windows Parental Controls',
-    '451' => 'Legally Restricted',
-    '500' => 'Internal Server Error',
-    '501' => 'Not Implemented',
-    '502' => 'Bad Gateway',
-    '503' => 'Service Unavailable',
-    '504' => 'Gateway Timeout',
-    '505' => 'HTTP Version Not Supported',
-    '506' => 'Variant Also Negotiates',
-    '507' => 'Insufficient Storage',
-    '509' => 'Bandwidth Limit Exceeded',
-    '510' => 'Not Extended'
-  }
+  codes = YAML::load_file('codes.yml')
 
   get '/' do
     @codes = codes
@@ -76,7 +30,7 @@ class MockIssues < Sinatra::Base
   end
 
   any '/:code' do
-    code = params[:code]
+    code = params[:code].to_i
     message = codes[code]
 
     if message.nil?
