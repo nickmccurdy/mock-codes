8b154ac6e5cf2f2444ee39d306c56bf9ef5931d5
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-10 18:10:10 -0400

    Use RSpec's new expect syntax

--- spec/mock_issues_spec.rb
+++ spec/mock_issues_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe MockIssues do
   it 'loads the home page' do
     get '/'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   each_code do |code, text|
     it "responds with #{code} (#{text})" do
       each_verb "/#{code}" do
-        last_response.status.should == code
+        expect(last_response.status).to eq code
         expected_body = (code == 304) ? '' : text
-        last_response.body.should == expected_body
+        expect(last_response.body).to eq expected_body
       end
     end
   end
@@ -19,23 +19,23 @@ describe MockIssues do
   it 'responds with 200 (Successful) after hesitating' do
     # 5 second delay
     each_verb '/hesitate' do
-      last_response.should be_ok
-      last_response.body.should == '5 seconds later...'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq '5 seconds later...'
     end
   end
 
   it 'responds with 200 (Successful) after timing out' do
     # 28 second delay
     each_verb '/timeout' do
-      last_response.should be_ok
-      last_response.body.should == '28 seconds later...'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq '28 seconds later...'
     end
   end
 
   it 'responds with 404 (Not Found) when a page does not exist' do
     each_verb '/not_a_valid_status_code' do
-      last_response.status.should == 404
-      last_response.body.should == 'Not Found'
+      expect(last_response.status).to eq 404
+      expect(last_response.body).to eq 'Not Found'
     end
   end
 end
