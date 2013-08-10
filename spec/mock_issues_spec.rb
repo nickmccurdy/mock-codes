b859885ac5dedc245ae4bf3389d4ebe067cbea43
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-09 21:33:52 -0400

    Don't use "should" in RSpec tests

--- spec/mock_issues_spec.rb
+++ spec/mock_issues_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe MockIssues do
-  it 'should load the home page' do
+  it 'loads the home page' do
     get '/'
     last_response.should be_ok
   end
 
   CODES.each do |code, text|
-    it "should respond with #{code} (#{text})" do
+    it "responds with #{code} (#{text})" do
       get "/#{code}"
       last_response.status.should == code
       expected_body = (code == 304) ? '' : text
@@ -15,21 +15,21 @@ describe MockIssues do
     end
   end
 
-  it 'should respond with 200 (Successful) after hesitating' do
+  it 'responds with 200 (Successful) after hesitating' do
     # 5 second delay
     get '/hesitate'
     last_response.should be_ok
     last_response.body.should == '5 seconds later...'
   end
 
-  it 'should respond with 200 (Successful) after timing out' do
+  it 'responds with 200 (Successful) after timing out' do
     # 28 second delay
     get '/timeout'
     last_response.should be_ok
     last_response.body.should == '28 seconds later...'
   end
 
-  it 'should respond with 404 (Not Found) when a page does not exist' do
+  it 'responds with 404 (Not Found) when a page does not exist' do
     get '/not_a_valid_status_code'
     last_response.status.should == 404
     last_response.body.should == 'Not Found'
