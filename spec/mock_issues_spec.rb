4840d19b039b73f1660cdaf6d4a5a5aa5c0fcaca
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-11 19:31:44 -0400

    Use describe and context blocks to organize the specs

--- spec/mock_issues_spec.rb
+++ spec/mock_issues_spec.rb
@@ -1,41 +1,55 @@
 require 'spec_helper'
 
 describe MockIssues do
-  it 'loads the home page' do
-    get '/'
-    expect(last_response).to be_ok
+  describe 'home page' do
+    it 'loads succesfully' do
+      get '/'
+      expect(last_response).to be_ok
+    end
   end
 
-  each_code do |code, text|
-    it "responds with #{code} (#{text})" do
-      each_verb "/#{code}" do
-        expect(last_response.status).to eq code
-        expected_body = (code == 304) ? '' : text
-        expect(last_response.body).to eq expected_body
+  describe 'status page' do
+    context 'for numerical codes' do
+      each_code do |code, text|
+        it "responds with #{code} (#{text})" do
+          each_verb "/#{code}" do
+            expect(last_response.status).to eq code
+            expected_body = (code == 304) ? '' : text
+            expect(last_response.body).to eq expected_body
+          end
+        end
+      end
+    end
+
+    context 'for time delays' do
+      context 'after hesitating' do
+        it 'responds with 200 (Successful)' do
+          # 5 second delay
+          each_verb '/hesitate' do
+            expect(last_response).to be_ok
+            expect(last_response.body).to eq '5 seconds later...'
+          end
+        end
+      end
+
+      context 'after timing out' do
+        it 'responds with 200 (Successful)' do
+          # 28 second delay
+          each_verb '/timeout' do
+            expect(last_response).to be_ok
+            expect(last_response.body).to eq '28 seconds later...'
+          end
+        end
       end
     end
   end
 
-  it 'responds with 200 (Successful) after hesitating' do
-    # 5 second delay
-    each_verb '/hesitate' do
-      expect(last_response).to be_ok
-      expect(last_response.body).to eq '5 seconds later...'
-    end
-  end
-
-  it 'responds with 200 (Successful) after timing out' do
-    # 28 second delay
-    each_verb '/timeout' do
-      expect(last_response).to be_ok
-      expect(last_response.body).to eq '28 seconds later...'
-    end
-  end
-
-  it 'responds with 404 (Not Found) when a page does not exist' do
-    each_verb '/not_a_valid_status_code' do
-      expect(last_response.status).to eq 404
-      expect(last_response.body).to eq 'Not Found'
+  describe '404 (Not Found) page' do
+    it 'displays when a page does not exist' do
+      each_verb '/not_a_valid_status_code' do
+        expect(last_response.status).to eq 404
+        expect(last_response.body).to eq 'Not Found'
+      end
     end
   end
 end
